8cdd6f95db708b7d210a5f3bbdbcb0c93d090f67
Tommaso <tommasobaccano@gmail.com>
2023-04-17 15:51:10 +0200

    Implementazione ProgrammaEventi

---
+++ csharp-gestore-eventi/ProgrammaEventi.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace csharp_gestore_eventi
+{
+	public class ProgrammaEventi
+	{
+        public ProgrammaEventi(string titolo)
+        {
+            Titolo = titolo;
+            Eventi = new List<Evento>();
+        }
+
+        public string Titolo { get; set; }
+        public List<Evento> Eventi { get; set; }
+
+        public void AddEvento(Evento evento)
+        {
+            Eventi.Add(evento);
+        }
+
+        public List<Evento> FilterByData(DateTime data)
+        {
+            List<Evento> res = new List<Evento>();
+            foreach (Evento evento in Eventi)
+            {
+                if (evento.Data == data)
+                {
+                    res.Add(evento);
+                }
+            }
+            return res;
+        }
+
+        public int GetProgramLenght()
+        {
+            return Eventi.Count;
+        }
+
+        public void ClearProgramList()
+        {
+            Eventi.Clear();
+        }
+
+        public void GetStringProgramma()
+        {
+            Console.WriteLine($"Titolo del programma: {Titolo}");
+            Console.WriteLine();
+            foreach (Evento evento in Eventi)
+            {
+                string res = evento.ToString();
+                Console.WriteLine(res.PadLeft(res.Length + 5));
+            }
+        }
+    }
+}
+
