b7fb664fd2ecbc04d742a777f8e7cb093d1d25cc
Tommaso <tommasobaccano@gmail.com>
2023-04-17 09:44:37 +0200

    Primo commit. Creazione classe Evento con attributi

---
+++ csharp-gestore-eventi/Evento.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace csharp_gestore_eventi
+{
+	public class Evento
+	{
+        public string Titolo { get; set; }
+        public DateTime Data { get; set; }
+        public int MaxCapienza { get; init; }
+        public int NumPrenotazioni { get; private set; }
+
+    }
+}
+
